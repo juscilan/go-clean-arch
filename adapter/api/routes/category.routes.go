1c071e55504d13d265e74f34adfce1b1c40c7e70
Juscilan Moreto <juscilan.moreto@xogito.com>
2023-02-17 20:53:11 -0300

    feat: list category is stable

--- adapter/api/routes/category.routes.go
+++ adapter/api/routes/category.routes.go
@@ -2,20 +2,21 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/juscilan/go-clean-arch/cmd/api/controllers"
-	"github.com/juscilan/go-clean-arch/internal/repositories"
+	"github.com/juscilan/go-clean-arch/adapter/api/controllers"
+	inmemorydb "github.com/juscilan/go-clean-arch/adapter/db/in-memory-db"
 )
 
 func CategoryRoutes(router *gin.Engine) {
-	categoryRoutes := router.Group("/categories")
 
-	inMemoryCategoryRepository := repositories.NewCategoryInMemoryRepository()
+	inMemoryCategoryRepository := inmemorydb.NewCategoryInMemoryRepository()
 
-	categoryRoutes.POST("/", func(ctx *gin.Context) {
+	rg := router.Group("/categories")
+
+	rg.POST("/", func(ctx *gin.Context) {
 		controllers.CreateCategoryController(ctx, inMemoryCategoryRepository)
 	})
 
-	categoryRoutes.GET("/", func(ctx *gin.Context) {
+	rg.GET("/", func(ctx *gin.Context) {
 		controllers.ListCategoryController(ctx, inMemoryCategoryRepository)
 	})
 
